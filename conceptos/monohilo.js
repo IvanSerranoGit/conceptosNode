33ddea0f746a95b350c3f0b2da910d0959977b88
Iván Serrano <contacto.ivanserrano@gmail.com>
2022-06-08 16:51:13 -0500

    Se envia monohilo.js

--- conceptos/monohilo.js
+++ conceptos/monohilo.js
@@ -1,1 +1,14 @@
 console.log("Hola, mundo");
+let i = 0;
+// recibe dos argumentos una funcion y el tiempo  en el que se va a ejecutar
+setInterval(function(){
+    console.log(i);
+    i++;
+
+    if (i === 5) {
+        console.log("forzamos error");
+        var a = 3 + z
+    }
+}, 1000)
+
+console.log("Segunda instrucciòn");
